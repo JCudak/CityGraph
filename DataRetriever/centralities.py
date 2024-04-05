7cbee8f0b16d8e9933ec138d2fd50456a39f969a
Karol Jurzec <85733696+karol-jurzec@users.noreply.github.com>
2024-04-05 02:45:26 +0200

    coloring nodes based on centrality measures

--- DataRetriever/centralities.py
+++ DataRetriever/centralities.py
@@ -1,5 +1,4 @@
 import networkx as nx
 
 def random_walk_betweenness(roads_graph):
-    rwb = nx.current_flow_betweenness_centrality(G=roads_graph.to_undirected())
-    return sorted(rwb.items(), key=lambda x: x[1], reverse=True)
+    return nx.current_flow_betweenness_centrality(G=roads_graph.to_undirected())
--- DataRetriever/main.py
+++ DataRetriever/main.py
@@ -1,3 +1,5 @@
+import matplotlib.colors as mcolors
+import matplotlib.cm as cm
 import osmnx as ox
 import folium
 import webbrowser
@@ -11,7 +13,25 @@ def retrieve_road_graph(place_name: str, network_type: str):
 def retrieve_graph(gdf_nodes, gdf_edges):
     return ox.graph_from_gdfs(gdf_nodes, gdf_edges)
 
-def create_map(nodes, edges):
+def color_nodes(c_measures):
+    colors = [(0, 1, 0), (1, 1, 0), (1, 0, 0)] #GREEN, YELLOW, RED 
+    cmap = mcolors.LinearSegmentedColormap.from_list('custom', colors)
+
+    max_betweenness = max(c_measures.values())
+    norm = mcolors.Normalize(vmin=0, vmax=max_betweenness)
+    cmap_scaled = cm.ScalarMappable(norm=norm, cmap=cmap)
+
+    node_colors = {node_id: mcolors.to_hex(cmap_scaled.to_rgba(betweenness_value)) 
+                   for node_id, betweenness_value in c_measures.items()}
+
+    #sns.heatmap(np.random.rand(5, 5), cmap=cmap)
+    #plt.xlabel('X-axis')
+    #plt.ylabel('Y-axis')
+    #plt.show()
+
+    return node_colors
+
+def create_map(nodes, edges, node_colors=None):
     folium_map = folium.Map(location=[nodes['y'].mean(), nodes['x'].mean()], zoom_start=15, tiles='cartodbpositron')
 
     for _, row in edges.iterrows():
@@ -24,11 +44,11 @@ def create_map(nodes, edges):
             arrow_head=2
         ).add_to(folium_map)
 
-    for _, row in nodes.iterrows():
+    for node_id, row in nodes.iterrows():
         folium.CircleMarker(
             location=(row['y'], row['x']),
             radius=2,
-            color='red',
+            color = 'red' if node_colors is None else node_colors[node_id],
             fill=True
         ).add_to(folium_map)
 
@@ -40,9 +60,9 @@ if __name__ == '__main__':
     network_type = "drive"  # "all_private", "all", "bike", "drive", "drive_service", "walk"
     nodes, edges = retrieve_road_graph(place_name, network_type)
     roads_graph = retrieve_graph(nodes, edges)
-
     rwb = random_walk_betweenness(roads_graph)
-    print(rwb)
+    node_colors = color_nodes(rwb)
 
-    create_map(nodes, edges)
+    #create_map(nodes, edges)
+    create_map(nodes, edges, node_colors)
     webbrowser.open('map.html')
