89b043d2c70d6ba064c30faab26cf78730ab2cf2
Karol Jurzec <85733696+karol-jurzec@users.noreply.github.com>
2024-05-11 01:23:12 +0200

    added tracking for adding/removing edges + saving calculated centralities

--- DataRetriever/difference_graph.py
+++ DataRetriever/difference_graph.py
@@ -5,25 +5,29 @@ from centralities import page_rank
 from centralities import local_clustering_coefficient
 
 
-def _compute_centralities(type, old_graph, new_graph):
-    old_graph_measure = [tuple()]
+def _compute_centralities(type, old_graph, new_graph, old_measure):
+    old_graph_measure = [tuple()] if old_measure is None else old_measure
     new_graph_measure = [tuple()]
 
     match type:
         case 'Random Walk Betweenness':
-            old_graph_measure = random_walk_betweenness(old_graph)
+            if old_measure is None:
+                old_graph_measure = random_walk_betweenness(old_graph) 
             new_graph_measure = random_walk_betweenness(new_graph)
 
         case 'Centrality Betweenness':
-            old_graph_measure = centrality_betweenness(old_graph)
+            if old_measure is None:
+                old_graph_measure = centrality_betweenness(old_graph)
             new_graph_measure = centrality_betweenness(new_graph)
 
         case 'Page Rank':
-            old_graph_measure = page_rank(old_graph)
+            if old_measure is None:
+                old_graph_measure = page_rank(old_graph)
             new_graph_measure = page_rank(new_graph)
 
         case 'Local Clustering Coefficient':
-            old_graph_measure = local_clustering_coefficient(old_graph)
+            if old_measure is None:
+                old_graph_measure = local_clustering_coefficient(old_graph)
             new_graph_measure = local_clustering_coefficient(new_graph)
         case _:
             print('Operation is not supporeted')
@@ -37,42 +41,12 @@ def _compute_difference(old_measure, new_measure):
 
     diff_measure = {}
 
-    for id, measure in old_measure.items():
+    for id, _ in old_measure.items():
         diff_measure[id] = new_measure[id] - old_measure[id]
 
     return list(diff_measure.items())
 
-
-def _retrieve_deleted_edges(old_graph, new_graph):
-    old_edges = {edge: old_graph[edge[0]][edge[1]] for edge in old_graph.edges}
-    new_edges = {edge: new_graph[edge[0]][edge[1]] for edge in new_graph.edges}
-
-    deleted_edges = {}
-
-    for edge, data in old_edges.items():
-        if edge not in new_edges:
-            deleted_edges[edge] = data
-
-    return deleted_edges
-
-
-def _retrieve_added_edges(old_graph, new_graph):
-    old_edges = {edge: old_graph[edge[0]][edge[1]] for edge in old_graph.edges}
-    new_edges = {edge: new_graph[edge[0]][edge[1]] for edge in new_graph.edges}
-
-    added_edges = {}
-
-    for edge, data in new_edges.items():
-        if edge not in old_edges:
-            added_edges[edge] = data
-
-    return added_edges
-
-
-def retrieve_difference_graph(old_graph, new_graph, measure_method):
-    old_measure, new_measure = _compute_centralities(measure_method, old_graph, new_graph)
+def retrieve_difference_graph(old_graph, new_graph, measure_method, old_measure = None):
+    old_measure, new_measure = _compute_centralities(measure_method, old_graph, new_graph, old_measure)
     diff_measure = _compute_difference(old_measure, new_measure)
-    deleted_edges = _retrieve_deleted_edges(old_graph, new_graph)
-    added_edges = _retrieve_added_edges(old_graph, new_graph)
-
-    return diff_measure, deleted_edges, added_edges
+    return diff_measure
